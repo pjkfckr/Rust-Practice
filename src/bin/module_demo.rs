f874e1ba118df8728a871d272ce8c82199006e04
Park Sangyeong <61726800+pjkfckr@users.noreply.github.com>
2023-01-30 21:29:32 +0900

    module_demo

---
+++ src/bin/module_demo.rs
@@ -0,0 +1,34 @@
+use crate::greet::hello;
+use std::collections::HashMap;
+
+mod greet {
+    use std::collections::HashMap;
+    pub(crate) fn hello() {
+        println!("hello");
+    }
+
+    pub(crate) fn goodbye() {
+        println!("goodbye");
+    }
+}
+
+mod math {
+    pub(crate) fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    pub(crate) fn sub(a: i32, b: i32) -> i32 {
+        a - b
+    }
+}
+
+
+
+fn main() {
+    use greet::hello;
+    hello();
+    greet::goodbye();
+    math::add(1, 1);
+    math::sub(2, 1);
+
+}
